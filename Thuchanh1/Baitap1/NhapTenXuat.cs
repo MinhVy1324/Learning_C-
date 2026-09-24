c92ed6495e44d12771b206282ed5156f5207cc56
MinhVy1324 <vominhvy0410@gmail.com>
2026-09-25 00:33:02 +0700

    xuất tên bằng lệnh cơ bản

---
+++ Thuchanh1/Baitap1/NhapTenXuat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyLib;
+namespace Baitap1
+{
+    class NhapTenXuat
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap ho ten: ");
+            string hoTen = Console.ReadLine();
+            string ketQua = NhapTenXuatLib.XuatHoTen(hoTen);
+            Console.WriteLine(ketQua);
+        }
+    }
+}
---
+++ Thuchanh1/MyLib/NhapTenXuatLib.cs
@@ -0,0 +1,10 @@
+﻿namespace MyLib
+{
+    public class NhapTenXuatLib
+    {
+        public static string XuatHoTen(string hoTen)
+        {
+            return $"Ho ten: {hoTen}";
+        }
+    }
+}
