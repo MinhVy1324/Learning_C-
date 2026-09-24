a2d8e0363c9bede75a6172b502634d640e6763e4
MinhVy1324 <vominhvy0410@gmail.com>
2026-09-25 00:49:07 +0700

    tinh luy thua

---
+++ Thuchanh1/Baitap2/LuyThua.cs
@@ -0,0 +1,28 @@
+﻿/*
+* THƯ VIỆN TÍNH LŨY THỪA X^Y
+* Tác giả : Võ Minh Vỹ
+*
+* Ý tưởng:
+*   Sử dụng hàm Math.Pow(x, y) để tính lũy thừa và ép kiểu về long.
+*
+* Mã giả:
+*   Hàm TinhLuyThua(x, y):
+*     Trả về (long)Math.Pow(x, y)
+*/
+using System;
+using MyLib;
+namespace Baitap2
+{
+    class LuyThua
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap so nguyen x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so nguyen y: ");
+            int y = int.Parse(Console.ReadLine());
+            long ketQua = LuyThuaLib.LuyThua(x, y);
+            Console.WriteLine($"{x}^{y} = {ketQua}");
+        }
+    }
+}
---
+++ Thuchanh1/MyLib/LuyThuaLib.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib
+{
+    public class LuyThuaLib
+    {
+        public static long LuyThua(int x,int y)
+        {
+            return (long)Math.Pow(x, y);
+        }
+    }
+}
